59dc1cab68e4d0d014bf2baaea1b6ca829f9ed84
hyunho <mizzking75@gmail.com>
2023-06-15 23:25:47 +0900

    Initial Commit

---
+++ DontGoMart/DontGoMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontGoMartApp.swift
+//  DontGoMart
+//
+//  Created by hyunho lee on 2023/06/15.
+//
+
+import SwiftUI
+
+@main
+struct DontGoMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
