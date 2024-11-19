02d09f4cbcdf2ebd2be0fe30d7bfdd19a7f8b1c2
HwangSukHyeon <payback9488@gmail.com>
2024-11-19 19:22:41 +0900

    테스트 코드 추가
    - SwiftTest 추가
    - 정해진 날짜와 비교했을 때, 맞는지 확인하는 함수

--- DontGoMart/DontGoMartApp.swift
+++ DontGoMart/DontGoMartApp.swift
@@ -66,7 +66,7 @@ struct DontGoMartApp: App {
 //        return calendar.date(from: components)
 //    }
     
-    private func generateBiweeklyTasks(
+    func generateBiweeklyTasks(
             forYear year: Int,
             monthRange: Range<Int> = 1..<13,
             weekdays: [(Calendar.Weekday, Calendar.Ordinal, String)],
@@ -87,7 +87,7 @@ struct DontGoMartApp: App {
             return tasks
         }
 
-    private func findPatternDay(of weekday: Calendar.Weekday, ordinal: Calendar.Ordinal, inMonth month: Int, year: Int, calendar: Calendar) -> Date? {
+    func findPatternDay(of weekday: Calendar.Weekday, ordinal: Calendar.Ordinal, inMonth month: Int, year: Int, calendar: Calendar) -> Date? {
         // 날짜 컴포넌트 설정
         var dateComponents = DateComponents(year: year, month: month)
 
